888c9193ed535ea665f219f999cd4a27a786da51
sunshine <sunshine46582747@sina.com>
2015-05-10 20:03:30 +0800

    Add a top level activity to run unit test.

---
+++ src/com/test/ui/MainActivity.java
@@ -0,0 +1,60 @@
+package com.test.ui;
+
+import com.example.flyin.R;
+import com.example.flyin.SelectImageActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+	private TextView tv;
+	private ImageView iv;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+		tv = (TextView) findViewById(R.id.tv);
+		iv = (ImageView) findViewById(R.id.iv);
+		
+		tv.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent intent = new Intent(MainActivity.this, SelectImageActivity.class);
+				startActivityForResult(intent, 34);
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+	
+		if (requestCode == 34 && resultCode == RESULT_OK)
+		{
+			String result = data.getStringExtra("IMAGE_PATH");
+			iv.setImageBitmap(BitmapFactory.decodeFile(result));
+			
+			tv.setText(result);
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
